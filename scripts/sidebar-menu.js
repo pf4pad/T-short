69268047fb365855fa116087dd9cbf146040fefe
pf4pad <65763670+pf4pad@users.noreply.github.com>
2020-10-14 21:06:06 +0300

    sidebar menu

--- scripts/sidebar-menu.js
+++ scripts/sidebar-menu.js
@@ -1,13 +1,13 @@
-(function() {
+(function () {
   const openingBtn = document.querySelector(".sidebar__hamburger");
   const closingBtn = document.querySelector(".sidebar__close");
   const sidebar = document.querySelector(".sidebar");
 
-  openingBtn.addEventListener("click", function() {
+  openingBtn.addEventListener("click", function () {
     sidebar.classList.add("sidebar--opened");
   });
 
-  closingBtn.addEventListener("click", function() {
+  closingBtn.addEventListener("click", function () {
     sidebar.classList.remove("sidebar--opened");
   });
 })();
